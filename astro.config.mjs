bcdd1db0e10fb95dc926547f72013739838406db
cwy <cwy@elvs.chc.edu.tw>
2025-08-31 17:08:42 +0800

    npm create

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: '揚揚教學網--114學年',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
