7c1a4f769f5c968902a6889c2ae8167c4d7455e3
陳文揚 <91643792+cwyedu@users.noreply.github.com>
2026-06-17 13:48:30 +0800

    add summer retake page and rename winter retake content

--- astro.config.mjs
+++ astro.config.mjs
@@ -72,7 +72,8 @@ export default defineConfig({
 						{ label: '均質化', link: '/misc/gamebit/gamebit' },
 						{ label: '自己錄首歌', link: '/misc/recording' },
 						{ label: '用 VS Code 寫網頁', link: '/misc/vscode-web' },
-						{ label: '114寒假重補修', link: '/misc/retake114' },
+						{ label: '114寒假重補修', link: '/misc/retake114-winter' },
+						{ label: '114暑假重補修', link: '/misc/retake114-summer' },
 					],
 
 				},
